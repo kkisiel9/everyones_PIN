92ab8b7ea6efb4c2527c0d7981624c5492ddfbac
Pooja Murali <p.murali2002@gmail.com>
2025-02-09 14:12:14 +0000

    CREATE: new pincode file

---
+++ pin_code.py
@@ -0,0 +1,21 @@
+
+correct_pin = "1234"
+
+
+max_attempts = 3
+
+
+attempts = 0
+
+while attempts < max_attempts:
+    supplied_pin = input("Enter your PIN: ")
+    attempts += 1  # Increase attempt count
+
+    if supplied_pin == correct_pin:
+        print(f"Access granted! You entered the correct PIN in {attempts} attempt(s).")
+        break
+    else:
+        print(f"Incorrect PIN. Attempt {attempts} of {max_attempts}.")
+
+    if attempts == max_attempts:
+        print("Too many failed attempts. Access denied.")
