a9c4e0f4581b489a36f394fc976924b6e99ed210
Pooja Murali <p.murali2002@gmail.com>
2025-02-11 16:39:04 +0000

    EDIT: added more comments to pooja code

--- pin_code.py
+++ pin_code.py
@@ -3,27 +3,31 @@
 correct_pin = "1234"
 
 # this is another variable to show the number of maximum attempts which is an integer.
-max_attempts = 3
+max_attempts_count = 3
 
 # this variable shows the current amount of attempts
-attempts = 0
+current_attempt_count = 0
 
 # when the current attempt is less than the maximum the attempts get increased by 1 and the input function is shown so you can enter the pin in
-while attempts < max_attempts:
-    # this uses the input function and also creates a variable for the inputted pin number
-    supplied_pin = input("Enter your PIN: ")
-    attempts += 1
+while current_attempt_count < max_attempts_count:
+    # i have created a variable for the inputted pin
+    # input function prompts the user to enter their pin and no need to use print function
+    pin_input = input("Enter your PIN: ")
+    current_attempt_count += 1
 
     # if the input number is the same as the actual pint a print message is printed with the number of actual attempts it took.
-    if supplied_pin == correct_pin:
-        print(f"Access granted! You entered the correct PIN in {attempts} attempt(s).")
+    # == double equals needs to be used as this is a comparison operator
+    # single = just assigns in python
+    if pin_input == correct_pin:
+        print(f"Access granted! You entered the correct PIN in {current_attempt_count} attempt(s).")
         # break statement ensure this loop keeps running until max attempts is reached
         break
+    # this gets printed whe you enter the incorrect pin but are still within the max attempts
     else:
-        print(f"Incorrect PIN. Attempt {attempts}")
+        print(f"Incorrect PIN. Attempt {current_attempt_count}")
 
-    # if the max attempts is exceeded it print this
-    if attempts == max_attempts:
+    # if the max attempts is exceeded the program print this
+    if current_attempt_count == max_attempts_count:
         print("Too many failed attempts. Access denied.")
 
 # this is an example of conditionals where break and its friends are used
